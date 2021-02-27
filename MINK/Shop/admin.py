c826215d4aef4df19fe33d2c92ff898834accd00
Isha Dhabalia <isha.dhabalia@somaiya.edu>
2021-02-28 04:29:09 +0530

    Shopping part Updated

--- MINK/Shop/admin.py
+++ MINK/Shop/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from.models import Product
+# Register your models here.
+from .models import Product, Contact, Orders
 
 admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(Orders)
--- MINK/Shop/views.py
+++ MINK/Shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact, Orders
 from math import ceil
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+# Create your views here.
+from django.http import HttpResponse
 
 
 def home(request):
@@ -40,5 +46,20 @@ def productView(request,myid):
     return render(request, 'Shop/prodView.html',{'product': product[0]})
 
 def checkout(request):
+    if request.method=="POST":
+        items_json= request.POST.get('itemsJson', '')
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        address=request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city=request.POST.get('city', '')
+        state=request.POST.get('state', '')
+        zip_code=request.POST.get('zip_code', '')
+        phone=request.POST.get('phone', '')
+
+        order = Orders(items_json= items_json, name=name, email=email, address= address, city=city, state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank=True
+        id=order.order_id
+        return render(request, 'Shop/checkout.html', {'thank':thank, 'id':id})
     return render(request, 'Shop/checkout.html')
 
